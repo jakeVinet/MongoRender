f0a8008e40f6e47c3caf919df4ea2d7aef6d095e
Jake Vinet <89436877+jakeVinet@users.noreply.github.com>
2024-02-29 20:39:42 +0000

    Actually the final commit

--- testmongo.js
+++ testmongo.js
@@ -31,7 +31,7 @@ app.get('/say/:name', function(req, res) {
 // Route to access database:
 app.get('/api/mongo/:item', function(req, res) {
 const client = new MongoClient(uri);
-const searchKey = "{ partID: '" + req.params.item + "' }";
+const searchKey = "{ name: '" + req.params.item + "' }";
 console.log("Looking for: " + searchKey);
 
 async function run() {
@@ -42,7 +42,7 @@ async function run() {
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
     // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+    const query = { name: req.params.item };
 
     const part = await parts.findOne(query);
     console.log(part);
